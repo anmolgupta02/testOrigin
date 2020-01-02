dcf86ee1b63294f7d38071ccde498135924e7495
anmol <anmolgupta02@gmail.com>
2020-01-02 18:12:35 +0530

    Navigation BL Implementation high level

--- Vistra_Filing_Regression/src/test/java/businessLogics/NavigationsBL.java
+++ Vistra_Filing_Regression/src/test/java/businessLogics/NavigationsBL.java
@@ -1,5 +1,44 @@
 package businessLogics;
 
-public class NavigationsBL {
+import org.openqa.selenium.WebElement;
 
+import pages.NavigationPL;
+import utils.DriverClass;
+import utils.HelperMethods;
+
+public class NavigationsBL extends DriverClass{
+	
+	HelperMethods hm = new HelperMethods();
+	WebElement placeholder = null;
+	NavigationPL npl;
+	
+	public void NavigateToSheftList() {
+		hm.clickOnElement(placeholder);	
+	}
+	
+	public void NavigateToSpInList() {
+		hm.clickOnElement(placeholder);
+	}
+	
+	public void NavigateToALF() {
+		hm.clickOnElement(placeholder);
+	}
+
+	public void NavigateToSTI() {
+		hm.clickOnElement(placeholder);
+	}
+	
+	
+	public void NavigateToShelfList() {
+		hm.clickOnElement(placeholder);
+	}
+	
+	public void NavigateToSINC() {
+		hm.clickOnElement(placeholder);
+	}
+	
+	public void NavigateToTS() {
+		hm.clickOnElement(placeholder);
+	}
+	
 }
